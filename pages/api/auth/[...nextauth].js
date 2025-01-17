da7d6cc3b03bd2251c935510f4c3f92c86110616
shyam shekar <shyamshekar@shyams-MacBook-Pro.local>
2025-01-17 10:55:13 +0100

    vercel test3

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,19 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+const githubuser = "Ov23liGBWLqW6dubedw0";
+const gitsecret = "7d7420262b3c3b2705cc4abd3e376ee00063432e";
+const mysecret = "f2e4a1d8c6f04b2a85c7e1d6c7e4b5a8f1d2c3e4f5a7c9e0d1b6f4e3c2a5d8b1";
+
+
 
 export const authOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.NEXT_SECRET,
-      clientSecret: process.env.NEXT_CLIENT,
+      clientId: githubuser,
+      clientSecret: gitsecret,
     }),
   ],
-  secret: process.env.NEXT_AUTHSECRET, // Add a secret for signing cookies
+  secret: mysecret, // Add a secret for signing cookies
 };
 
 export default NextAuth(authOptions);
