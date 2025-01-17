6d36b8468e93e32761ee3c9e0ba740f42efc01e7
shyam shekar <shyamshekar@shyams-MacBook-Pro.local>
2025-01-17 11:33:09 +0100

    vercel test3

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,10 +5,12 @@ import GitHubProvider from "next-auth/providers/github";
 // const secretnextkey = "f2e4a1d8c6f04b2a85c7e1d6c7e4b5a8f1d2c3e4f5a7c9e0d1b6f4e3c2a5d8b1";
 
 
-const Githublogin = process.env.NEXT_CLIENT;
-const Githubpass = process.env.NEXT_SECRET;
-const secretnextkey = process.env.NEXT_AUTHSECRET;
-
+const Githublogin = process.env.NEXT_PUBLIC_BASE_URL2
+;
+const Githubpass = process.env.NEXT_PUBLIC_BASE_URL3
+;
+const secretnextkey = process.env.NEXT_PUBLIC_BASE_URL4
+;
 export const authOptions = {
   providers: [
     GitHubProvider({
